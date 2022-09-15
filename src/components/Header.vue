a4e65383806ab1034b42ebd2ea3246f2668a6ea3
JuanAlmeida25 <almeidajuan3@gmail.com>
2022-09-15 12:55:46 -0500

    navegacion con animacion funcional

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,22 @@
-
-
 <template>
   <div id="barra" class="barra-navegacion">
-    <div id="animacion" class="animacion"></div>
+    
     <nav class="barra">
-      <a id="a1" href="#" @mouseover="hover_mover('a1')">ARTICLES</a>
-      <a id="a2" href="#" @mouseover="hover_mover('a2')">BENEFITS</a>
-      <a id="a3" href="#" @mouseover="hover_mover('a3')">TOKENOMICS</a>
-      <a id="a4" href="#" @mouseover="hover_mover('a4')">NEWS</a>
-      <a id="a5" href="#" @mouseover="hover_mover('a5')">ROADMAPS</a>
-      <a id="a6" href="#" @mouseover="hover_mover('a6')">SEARCH</a>
+
+      <a id="a1" href="#" @mouseover="hover_mover('a1')">articels</a>
+      <a id="a2" href="#" @mouseover="hover_mover('a2')">benefits</a>
+      <a id="a3" href="#" @mouseover="hover_mover('a3')">tokenomics</a>
+      <a id="a4" href="#" @mouseover="hover_mover('a4')">news</a>
+      <a id="a5" href="#" @mouseover="hover_mover('a5')">roadmaps</a>
+      <a id="a6" href="#" @mouseover="hover_mover('a6')">search</a>
+      <div id="animacion" class="animacion"></div>
+    
     </nav>
 
     <button class="btn" @click="increment">WHITEPAPER</button>
   </div>
 </template>
+
 <script>
 function getOffset(el) {
   var _x = 0;
@@ -36,24 +38,25 @@ export default {
   },
   methods: {
     hover_mover(id) {
-      if (this.animando) {
-        return;
-      }
-      var anim = document.getElementById("animacion");
-      var link = document.getElementById(id);
-      var distancia_recorrer = link.offsetLeft - this.distancia;
-      anim.style.left = distancia_recorrer + "px";
+      let  anim = document.getElementById("animacion");
+      let  link = document.getElementById(id);
+      anim.style.minWidth = link.offsetWidth + "px";
+      anim.style.left = getOffset(link).left + "px";
+   
     },
   },
   mounted() {
-    this.distancia = getOffset(document.getElementById("animacion")).left;
+
     var anim = document.getElementById("animacion");
     var link = document.getElementById("a1");
-    anim.style.left = link.offsetLeft - this.distancia + "px";
+    anim.style.minWidth = link.offsetWidth + "px";
+    anim.style.left =  getOffset(anim).left + "px";
+  
   },
 };
 </script>
-<style >
+
+<style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap");
 .barra-navegacion {
   position: fixed;
@@ -85,20 +88,26 @@ export default {
   align-items: center;
   font-family: "Montserrat", sans-serif;
   font-size: 1rem;
+  text-transform: uppercase;
   color: white;
   text-decoration: none;
   height: 70%;
   margin: 0rem;
   padding: 0.4rem;
   border-radius: 0.4rem;
+  z-index:1;
 }
 .animacion {
-  position: relative;
+  position: absolute;
+  
   /* left: 60rem; */
-  width: 5rem;
+  min-width: 1rem;
+  width: 2rem;
   height: 70%;
   border-radius: 0.4rem;
   background-color: #ffc000;
+  z-index:0;
+  transition: all .4s ease 0s;
 }
 /* .barra a:hover {
   background-color: #ffc000;
