58c058954038c7c9358f88c14b0952bf81063da7
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-09-17 15:15:18 -0500

    organizar los elementos a mostrar

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,42 @@ import Bienvenida from "./components/Bienvenida.vue";
   </header>
 
   <main>
-  <Bienvenida/>
-    <Info/>
+
+    <!-- 
+    para agregar una pantalla agreguela dentro de uno de los div libre
+    sino hay div libre, cree uno con el siguiente #Id respetando la secuencia, por ejemplo
+    si el ultimo div tiene el id 5, entonces el que usted cree, debe ser el id 6
+    y dentro de su div van los componentes, puede eliminar las clases 
+    guiese del ejemplo div 1
+    -->
+    <div id="1">
+      <Bienvenida />
+      <Info />
+    </div>
+    <div id="2" class="container red"></div>
+    <div id="3" class="container yellow"></div>
+    <div id="4" class="container green"></div>
+    <div id="5" class="container orange"></div>
   </main>
 </template>
+<script>
+let id = 1
 
+document.addEventListener('wheel', function () {
+  document.getElementById(id).style.visibility = "hidden"
+  if (event.deltaY > 0) {
+    if (id < 5)
+      id = id + 1
+  } else {
+    if (id > 1)
+      id = id - 1
+  }
+  document.getElementById(id).style.visibility = "visible"
+  location.hash = "#" + id;
+  window.history.pushState({}, document.title, window.location.pathname);
+
+});
+</script>
 <style>
 * {
   margin: 0;
@@ -24,6 +55,12 @@ import Bienvenida from "./components/Bienvenida.vue";
   text-decoration: none;
   border: none;
   outline: none;
+
+}
+
+#app,
+main {
+  height: 100%;
 }
 
 html,
@@ -34,16 +71,47 @@ body {
   overflow: hidden;
   background-color: #333;
 }
+
 ::-webkit-scrollbar {
   display: none;
 }
+
 header,
 Header {
   width: 100%;
   position: fixed;
   z-index: 999;
 }
+
 main {
   z-index: 0;
+}
+
+/* Style cambiar de div */
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.orange {
+  background-color: orange;
+}
+
+.red {
+  background-color: red;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+
+.container {
+  padding: 0%;
+  margin: 0%;
+  height: 100%;
 }
 </style>
