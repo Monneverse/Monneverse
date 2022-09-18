67414043b979fd7c20294cb7531e46d26f2c25d7
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-09-17 23:59:04 -0500

    Vista Alliances y Componentes

---
+++ src/components/Alliances.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="upcoming_alliance"><h1>UPCOMING ALLIANCES</h1></div>
+    <div class="visa_img">
+      <img src="/img/Alianzas/visa.png" />
+    </div>
+    <div class="mastercard_img">
+      <img src="/img/Alianzas/mastercard.png" />
+    </div>
+    <div class="reddit">
+      <a href="#"><img src="/public/img/Alianzas/reddit.png" /></a>
+      <!-- <font-awesome-icon icon="fa-brands fa-reddit" /> -->
+    </div>
+    <div class="telegram">
+      <a href="#"><img src="/img/Alianzas/telegram.png" /></a>
+      <!-- <font-awesome-icon icon="fa-brands fa-telegram" /> -->
+    </div>
+    <div class="discord">
+      <a href="#"><img src="/img/Alianzas/discord.png" /></a>
+      <!-- <font-awesome-icon icon="fa-brands fa-discord" /> -->
+    </div>
+  
+    <div class="semi_circle">
+      <img src="/img/semi_circulo.png" />
+    </div>
+    <div class="circle">
+      <img src="/img/Semi_circulo1.png" />
+    </div>
+  
+    <!--FONDO-->
+    <div class="fondo fondo-calculadora"></div>
+    <div class="fondo filtro-superior"></div>
+    <div class="fondo fondo-montana"><img src="/img/fondo-montana.png" /></div>
+    <div class="fondo fondo-oscurecer"></div>
+    <div class="circuito-izquierdo">
+      <img src="/img/calculadora/circuito-izquierdo.png" alt="" />
+    </div>
+    <div class="circuito-derecho">
+      <img src="/img/calculadora/circuito-derecho.png" alt="" />
+    </div>
+    <!--FIN FONDO-->
+  </template>
