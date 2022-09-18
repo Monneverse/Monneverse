a54abca658b7f1a93807a0e3aff6d09b0454207c
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-09-18 16:58:15 -0500

    agregar redes

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -2,7 +2,8 @@
 <template>
   <div>
     <div class="logo">
-        <img src="/img/logo.svg" alt="logo monneverse"></div>
+      <img src="/img/logo.svg" alt="logo monneverse">
+    </div>
     <div class="fondo fondo-calculadora"></div>
     <div class="fondo filtro-superior"></div>
     <div class="fondo fondo-montana">
@@ -63,9 +64,9 @@
       </svg>
     </div>
     <p id="month-1">1st Month</p>
-      <p id="month-2">2st Month</p>
+    <p id="month-2">2st Month</p>
     <div class="form-controles">
-   
+
       <p></p>
       <div class="inversion">
         <input type="number" name="inversion" id="inversion" placeholder="$ 100">
@@ -79,7 +80,20 @@
       </div>
     </div>
     <div class="logo-redes">
-      <font-awesome-icon icon="fa-brands fa-telegram" size="10x" />
+      <div class="icon icon-telegram">
+        <font-awesome-icon icon="fa-brands fa-telegram" />
+        <div class="fondo-icono"></div>
+        
+      </div>
+      <div class="icon icon-reddit">
+        <font-awesome-icon icon="fa-brands fa-reddit" />
+        <div class="fondo-icono"> </div>
+        
+      </div>
+      <div class="icon icon-discord">
+        <font-awesome-icon icon="fa-brands fa-discord" />
+        
+      </div>
     </div>
   </div>
 </template>
@@ -296,7 +310,8 @@ img {
   width: 22%;
   min-width: 22rem;
 }
-#month-1{
+
+#month-1 {
   font-family: 'Work Sans', sans-serif;
   color: white;
   font-size: 1.5rem;
@@ -305,7 +320,8 @@ img {
   left: 47%;
   z-index: 11;
 }
-#month-2{
+
+#month-2 {
   font-family: 'Work Sans', sans-serif;
   color: white;
   font-size: 1.5rem;
@@ -314,24 +330,64 @@ img {
   left: 65%;
   z-index: 11;
 }
-.logo-redes{
+
+.logo-redes {
+  position: absolute;
+  top: 90%;
+  left: 1rem;
+  width: 20rem;
+  height: 5rem;
+  z-index: 14;
+  display: flex;
+  justify-content: start;
+}
+
+.logo {
   position: relative;
-    top: 10rem;
-    left: 20rem;
-    width: 5rem;
-    height: 5rem;
-    z-index: 14;
+  top: 2rem;
+  left: 2rem;
+  width: 5rem;
+  height: 5rem;
+  z-index: 12;
+}
+.icon{
+  font-size: 4rem;
+  z-index: 18;
+  margin-left: 1rem;
 }
 
-.logo{
-    position: relative;
-    top: 2rem;
-    left: 2rem;
-    width: 5rem;
-    height: 5rem;
-    z-index: 12;
+.fa-telegram {
+  position: relative;
+  color: #5CBAE7;
+  z-index: 18;
 }
-
+.fa-reddit{
+  position: relative;
+  color: #FF4500;
+  z-index: 18;
+}
+.fondo-icono {
+  position: relative;
+  top: -75%;
+  left: 20%;
+  width: 60%;
+  height: 50%;
+  background-color: white;
+  z-index: 17;
+}
+.icon-discord{
+  border-radius: 50%;
+  background-color: #5865F2;
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.fa-discord{
+  color: #ffffff;
+  font-size: 2rem;
+}
 </style>
 
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import {faTelegram} from "@fortawesome/free-brands-svg-icons";
+import {faTelegram,faReddit, faDiscord} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faPhone,faTelegram);
+library.add(faPhone,faTelegram,faReddit,faDiscord);
 
 createApp(App)
 .component("font-awesome-icon", FontAwesomeIcon)
