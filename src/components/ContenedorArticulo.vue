1516890027118a7f9b848d9a96eb0c0ebcd66ae8
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-09-25 10:54:29 -0500

    eliminar console,log

--- src/components/ContenedorArticulo.vue
+++ src/components/ContenedorArticulo.vue
@@ -102,7 +102,6 @@ export default {
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      console.log(seconds);
       this.diaString = this.AgregarCero(days)
       this.horaString = this.AgregarCero(hours)
       this.minutoString = this.AgregarCero(minutes)
