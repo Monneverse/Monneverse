610aca086b237da8e50925f4aba58d2d521cb796
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-10-06 09:15:25 -0500

    Coins v1

---
+++ src/components/Coins.vue
@@ -0,0 +1,209 @@
+<script setup>
+import coin from "./componentesCoins.vue";
+</script>
+
+<template>
+  <!--FONDO-->
+  <div class="fondo fondo-calculadora"></div>
+  <div class="fondo filtro-superior"></div>
+  <div class="fondo fondo-montana"><img src="/img/fondo-montana.png" /></div>
+  <div class="fondo fondo-oscurecer"></div>
+  <div class="circuito circuito-izquierdo">
+    <img src="/img/calculadora/circuito-izquierdo.png" alt="" />
+  </div>
+  <div class="circuito circuito-derecho">
+    <img src="/img/calculadora/circuito-derecho.png" alt="" />
+  </div>
+  <div class="luces">
+    <img src="/img/luz.png" alt="" />
+  </div>
+  <div class="degradado_azuliz"></div>
+  <div class="degradado_azulde"></div>
+  <!--FIN FONDO-->
+  <div class="contenedor_coins">
+    <div class="card_coin">
+      <coin
+        icono="/img/coins/kucoin.svg"
+        titulo="KuCoin token "
+        sigla="(KCS)"
+        texto="TOP 52 approx capitalization"
+        capitalizacion="900 M USD"
+        texto2="Each Monner DeFi (MRN) with this capitalization of KuCoin token (KCS) would have a value of"
+        logo="/img/logo.svg"
+        precio="$ 90.00"
+      />
+    </div>
+    <div class="card_coin">
+      <coin
+        icono="/img/coins/axie.svg"
+        titulo="Axie Infinity"
+        sigla="(AXS)"
+        texto="TOP 48 approx capitalization"
+        capitalizacion="1.05 B USD"
+        texto2="Each Monner DeFi (MRN) with this capitalization of AXIE INFINITY (AXS) would have a value of"
+        logo="/img/logo.svg"
+        precio="$ 90.00"
+      />
+    </div>
+    <div class="card_coin">
+      <coin
+        icono="/img/coins/monero.svg"
+        titulo="Decentraland"
+        sigla="(MANA)"
+        texto="TOP 42 approx capitalization"
+        capitalizacion="1.30 B USD"
+        texto2="Each Monner DeFi (MRN) with this capitalization of Decentraland (MANA) would have a value of"
+        logo="/img/logo.svg"
+        precio="$ 90.00"
+      />
+    </div>
+
+    <div class="card_coin">
+      <coin
+        icono="/img/coins/monero.svg"
+        titulo="Monero"
+        sigla="(XRM)"
+        texto="TOP 28 approx capitalization"
+        capitalizacion="2.68 B USD"
+        texto2="Each Monner DeFi (MRN) with this capitalization of  Monero (XRM) would have a value of"
+        logo="/img/logo.svg"
+        precio="$ 90.00"
+      />
+    </div>
+    <div class="card_coin">
+      <coin
+        icono="/img/coins/chain.svg"
+        titulo="Chainlink"
+        sigla="(LINK)"
+        texto="TOP 21 approx capitalization"
+        capitalizacion="3.72 B USD"
+        texto2="Each Monner DeFi (MRN) with this capitalization of Chainlink (LINK) would have a value of"
+        logo="/img/coins/logo.svg"
+        precio="$ 90.00"
+        caja="/img/coins/caja.png"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@100;400;500;600&family=Work+Sans:wght@300;400;500;600;700&display=swap');
+img {
+  width: 100%;
+  height: 100%;
+}
+
+/* INICIO ESTILO FONDO */
+.fondo {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+}
+
+.fondo-calculadora {
+  z-index: 0;
+  background: radial-gradient(
+    circle at center,
+    #48d0ab 0%,
+    #097561 50%,
+    #505863 100%
+  );
+  z-index: 1;
+}
+
+.filtro-superior {
+  z-index: 2;
+  background: radial-gradient(
+    circle at center,
+    #f9f9fa 0%,
+    #c8ced4 50%,
+    #a8aaaf 100%
+  );
+  mix-blend-mode: multiply;
+}
+
+.fondo-montana {
+  height: 100%;
+  top: 10%;
+  left: 0;
+  z-index: 3;
+}
+
+.fondo-oscurecer {
+  z-index: 4;
+  background-color: rgb(0, 0, 0);
+  opacity: 60%;
+  mix-blend-mode: multiply;
+}
+
+.luces {
+  position: absolute;
+  bottom: -12%;
+  left: -9%;
+  width: 120%;
+  height: 110%;
+  z-index: 5;
+  opacity: 100%;
+}
+
+.degradado_azuliz {
+  background: linear-gradient(to right, #08047a 0.5%, transparent);
+  position: absolute;
+  transform: rotate(-60deg);
+  top: 0%;
+  z-index: 7;
+  right: 50%;
+  width: 50%;
+  height: 180%;
+  opacity: 70%;
+}
+
+.degradado_azulde {
+  background: linear-gradient(to right, #08047a 0.5%, transparent);
+  position: absolute;
+  transform: rotate(230deg);
+  top: -12%;
+  left: 50%;
+  width: 50%;
+  height: 180%;
+  opacity: 70%;
+  z-index: 7;
+}
+
+.circuito {
+  position: absolute;
+  z-index: 8;
+  height: 80%;
+  width: 40%;
+  min-width: 30rem;
+  top: 40%;
+}
+
+.circuito-izquierdo {
+  left: -10%;
+}
+
+.circuito-derecho {
+  right: -10%;
+  transform: scaleY(-1);
+}
+
+/* FIN ESTILO FONDO */
+
+.contenedor_coins {
+  position: absolute;
+  width: 100%;
+  height: 90%;
+  bottom: 0;
+  margin: 0 auto;
+  z-index: 17;
+  display: flex;
+  justify-content: center;
+}
+.contenedor_coins .card_coin {
+  width: 20%;
+}
+</style>
---
+++ src/components/componentesCoins.vue
@@ -0,0 +1,155 @@
+<script>
+export default {
+  props: {
+    icono: {
+      type: String,
+      required: true,
+    },
+    titulo: {
+      type: String,
+      required: true,
+    },
+    sigla: {
+      type: String,
+      required: true,
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+    capitalizacion: {
+      type: String,
+      required: true,
+    },
+    texto2: {
+      type: String,
+      required: true,
+    },
+    logo: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: String,
+      required: true,
+    },
+    caja: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="coin">
+    <div class="icono">
+      <img :src="icono" alt="" />
+    </div>
+    <div class="container-titulo">
+      <h2 class="titulo">{{ titulo }}</h2>
+      <p class="sigla">{{ sigla }}</p>
+    </div>
+    <div>
+      <span>{{ texto }}</span>
+    </div>
+    <div class="container-capi">
+      <h1 class="capitalizacion">{{ capitalizacion }}</h1>
+    </div>
+    <div class="container-text2">
+      <p class="texto2">{{ texto2 }}</p>
+    </div>
+    <div class="logo">
+      <img :src="logo" alt="" />
+    </div>
+    <div class="container-precio">
+      <h2 class="precio">
+        {{ precio }}
+      </h2>
+      <div>
+        <img :src="caja" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@200;300;400;700&display=swap");
+
+.coin {
+  position: relative;
+  top: 10%;
+  width: 95%;
+  height: 100%;
+  margin: 0 auto;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Work Sans", sans-serif;
+}
+
+.icono {
+  width: 60%;
+  height: 10%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.icono img {
+  width: 50%;
+}
+.container-titulo {
+  width: 100%;
+  height: 10%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.container-titulo .titulo {
+  font-weight: 500;
+}
+.container-capi {
+  height: 10%;
+}
+.container-capi .capitalizacion {
+  color: rgb(18, 225, 142);
+  font-family: "Barlow Semi Condensed", sans-serif;
+  font-weight: 600;
+}
+
+.container-text2 {
+  height: 17%;
+  text-align: center;
+}
+.container-text2 .texto2 {
+  text-align: center;
+}
+
+.logo {
+  width: 60%;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.logo img {
+  width: 70%;
+  margin: 0 auto;
+}
+
+.container-precio{
+  width: 60%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+</style>
