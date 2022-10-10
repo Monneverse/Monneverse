3ad68ec01e0aa1deeb325ac7e62eada55f0064ea
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-10-10 14:12:57 -0500

    Ahora si

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,58 +1,90 @@
-
 <template>
-  <div :class="{logo: true , mostrar:isVisible}">
-    <a href="" :class="{link:true,home:indexF==6}" @mouseover="hover_mover('a0')" @click="this.setPage(0)">
-      <img src="/img/logo.svg" alt="logo monneverse"></a>
+  <div :class="{ logo: true, mostrar: isVisible }">
+    <a
+      href=""
+      :class="{ link: true, home: indexF == 6 }"
+      @mouseover="hover_mover('a0')"
+      @click="this.setPage(0)"
+    >
+      <img src="/img/logo.svg" alt="logo monneverse"
+    /></a>
   </div>
   <div id="barra" class="barra-navegacion">
-
-    
-      
-    <div class="content-nav">
-      
-    </div>
+    <div class="content-nav"></div>
     <nav class="barra" @mouseout="exit_mouse">
-      <a id="a1" :class="{link:true,home:indexF==1}" @mouseover="hover_mover('a1')"
-        @click="this.setPage(1)">articles</a>
-      <a id="a2" :class="{link:true,home:indexF==2}" @mouseover="hover_mover('a2')"
-        @click="this.setPage(2)">benefits</a>
-      <a id="a3" :class="{link:true,home:indexF==3}" @mouseover="hover_mover('a3')"
-        @click="this.setPage(3)">tokenomics</a>
-      <a id="a4" :class="{link:true,home:indexF==4}" @mouseover="hover_mover('a4')" @click="this.setPage(4)">news</a>
-      <a id="a5" :class="{link:true,home:indexF==5}" @mouseover="hover_mover('a5')"
-        @click="this.setPage(5)">roadmaps</a>
-      <div id="animacion" :class="{animacion:true, 'traslado_animacion':true ,invisible:indexF==0}"></div>
+      <a
+        id="a1"
+        :class="{ link: true, home: indexF == 1 }"
+        @mouseover="hover_mover('a1')"
+        @click="this.setPage(1)"
+        >articles</a
+      >
+      <a
+        id="a2"
+        :class="{ link: true, home: indexF == 2 }"
+        @mouseover="hover_mover('a2')"
+        @click="this.setPage(2)"
+        >benefits</a
+      >
+      <a
+        id="a3"
+        :class="{ link: true, home: indexF == 3 }"
+        @mouseover="hover_mover('a3')"
+        @click="this.setPage(3)"
+        >tokenomics</a
+      >
+      <a
+        id="a4"
+        :class="{ link: true, home: indexF == 4 }"
+        @mouseover="hover_mover('a4')"
+        @click="this.setPage(4)"
+        >news</a
+      >
+      <a
+        id="a5"
+        :class="{ link: true, home: indexF == 5 }"
+        @mouseover="hover_mover('a5')"
+        @click="this.setPage(5)"
+        >roadmaps</a
+      >
+      <div
+        id="animacion"
+        :class="{
+          animacion: true,
+          traslado_animacion: true,
+          invisible: indexF == 0,
+        }"
+      ></div>
     </nav>
+  </div>
 
+  <div class="container-whitepaper">
     <button class="btn" @click="reDirect">WHITEPAPER</button>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       distancia: 0,
       animando: false,
-      event: "click"
+      event: "click",
     };
   },
   props: {
-
     index: {
       type: Number,
-      required: false
+      required: false,
     },
     isVisible: {
       type: Boolean,
-      required: true
+      required: true,
     },
     setPage: {
       type: Function,
-      required: true
+      required: true,
     },
-
   },
   computed: {
     indexF: {
@@ -62,13 +94,11 @@ export default {
           var link = document.getElementsByClassName("link")[this.index - 1];
           anim.style.minWidth = link.offsetWidth + "px";
           anim.style.left = this.getOffset(link).left + "px";
-        } catch (error) {
+        } catch (error) {}
 
-        }
-
-        return this.index
-      }
-    }
+        return this.index;
+      },
+    },
   },
   methods: {
     hover_mover(id) {
@@ -106,16 +136,16 @@ export default {
       } catch (error) {
         console.log("EL ERROR ES " + error);
       }
-
     },
     reDirect() {
-      location.replace("https://sites.google.com/view/whiterpaper-monnerverse/p%C3%A1gina-principal");
-    }
+      location.replace(
+        "https://sites.google.com/view/whiterpaper-monnerverse/p%C3%A1gina-principal"
+      );
+    },
   },
   mounted() {
     this.recargar();
     window.addEventListener("resize", this.recargar);
-
   },
 };
 </script>
@@ -160,11 +190,11 @@ export default {
   width: 70%;
   height: 100%;
   max-width: 40rem;
-
 }
 
+
 .barra a {
-  position: relative;
+ 
   left: 0%;
   display: flex;
   justify-content: center;
@@ -189,20 +219,27 @@ export default {
   width: 2rem;
   height: 70%;
   border-radius: 0.4rem;
-  background: radial-gradient(circle at center,
-      #32987d 0%,
-      #097561 50%,
-      #505863 100%);
+  background: radial-gradient(
+    circle at center,
+    #32987d 0%,
+    #097561 50%,
+    #505863 100%
+  );
   z-index: 0;
 }
 
 .traslado_animacion {
   transition: all 0.4s ease 0s;
 }
-
+.container-whitepaper{
+  position:fixed;
+  width: 30%;
+  top: 5%;
+  left: 77%;
+  height: 40%;
+  
+}
 .btn {
-  position: relative;
-  left: 12%;
   font-family: "Montserrat", sans-serif;
   font-size: 16px;
   font-weight: bold;
@@ -212,6 +249,8 @@ export default {
   text-align: center;
   background-color: #ffc000;
   padding: 0.8rem;
+  width: 42%;
+  height: 15%;
   min-width: 8rem;
 }
 
@@ -220,7 +259,6 @@ export default {
 }
 
 @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) {
-
   .barra {
     display: none;
   }
