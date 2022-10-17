911417914d1e4ce43b1be986bb7785513542b6d7
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-10-18 08:03:59 +0900

    he vuelvo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{ logo: true, mostrar: isVisible }">
-    <a href="" :class="{home: indexF == 6 }">
+    <a href=""  @click="this.setPage(1)" >
       <img src="/img/logo.svg" alt="logo monneverse" /></a>
   </div>
   <div id="barra" class="barra-navegacion">
