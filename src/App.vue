ca9e7ec21f3c63c71288bf3b4f7606d09000af8b
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-10-18 09:20:49 +0900

    mostrar icono

--- src/App.vue
+++ src/App.vue
@@ -200,9 +200,9 @@ export default {
     };
   },
   methods: {
-    setOnPage(id) {
+    setOnPage(id, visible = true) {
       this.index = id;
-
+      this.isVisibleLogo = visible;
       switch (this.index) {
         case 1:
           this.index_pagina = 5;
