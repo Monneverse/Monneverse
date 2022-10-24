4a0ccb72002cdacf5ea78133a31e6469d824467c
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-10-24 18:00:35 -0500

    cambios de en nombre de los archivos de video

--- src/components/Bienvenida.vue
+++ src/components/Bienvenida.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- video de fondo -->
   <video autoplay="true" muted="true" loop="true" class="video-fondo" poster="/img/fondo_binario.png">
-    <source src="/video/Full HD Lite.mp4" type="video/mp4">
+    <source src="/video/Full-HD-Lite.mp4" type="video/mp4">
     <source src="/video/fondo-lite.mp4">
     <img src="/img/fondo_binario.png" />
   </video>
