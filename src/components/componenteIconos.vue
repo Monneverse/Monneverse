4db5910b4d49fd7cdca9ae871adf9773542fe239
Daniel Balasnoa <65874667+Daniel20020710@users.noreply.github.com>
2022-10-25 16:31:52 -0500

    Exchanges Movil pero o que costo

--- src/components/componenteIconos.vue
+++ src/components/componenteIconos.vue
@@ -15,52 +15,22 @@ defineProps({
 
 <template>
   <div class="contenedor_icono">
-    <div class="icono">
-      <img :src=iconos class="icon_coin" />
+    <div class="icon">
+      <img :src="iconos" class="icon_coin" />
     </div>
     <div class="name_coin">
-      <p>{{iconname}}</p>
+      <p>{{ iconname }}</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-img {
-  width: 100%;
-  height: 100%;
-}
-
 .contenedor_icono {
-  background: linear-gradient(to right,
-      #029F50 0%,
-      #0A664E 50%,
-      #0E444D 100%);
-  border-radius: 1.3rem;
-  position: absolute;
   display: flex;
-  align-items: center;
-  text-align: center;
-  margin: 0%;
-  left: 20%;
-  height: 70%;
-  z-index: 7;
-  width: 65%;
-  top: 0%;
-  gap: 2rem;
-}
-
-.icon_coin {
-  position: absolute;
-  top: 0%;
-  right: 15%;
-  width: 32%;
   height: 100%;
-  background: linear-gradient(to left,
-      #029F50 0%,
-      #0A664E 50%,
-      #0E444D 100%);
+  z-index: 7;
+  width: 100%;
 }
-
 /* .contenedor_icono:hover {
   width: 88%;
   transition: 0.5s;
@@ -70,35 +40,39 @@ img {
   width: 100%;
 } */
 
-.icono {
+.icon {
   transition: transform 0.75s;
-  position: relative;
   display: flex;
-  height: 100%;
+  height: 80%;
   width: 30%;
+  background-color: #161431;
+  border-radius: 1rem;
 }
 
 .icon_coin {
-  position: absolute;
-  border-radius: 1rem;
-  overflow: visible;
-  min-width: 4.3vw;
-  height: 100%;
+  margin: 4px auto;
+  width: 50%;
+  height: 80%;
   padding: 0%;
-  right: 120%;
 }
 
 .name_coin {
-  position: absolute;
+  background: linear-gradient(to left, #029f50 0%, #0a664e 50%, #0e444d 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.6em;
+  margin: 0 auto;
+  background-color: #029f50;
   font-family: Calibri;
   text-transform: uppercase;
-  text-align: center;
   left: 14%;
   font-weight: bold;
   font-size: 1.3vw;
-  float: center;
   color: white;
   text-align: center;
+  width: 100%;
+  height: 80%;
 }
 
 @media only screen and (min-device-width: 375px) and (max-device-width: 667px) {
