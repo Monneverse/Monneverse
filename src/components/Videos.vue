3338e71f85666c7a4ca1bdd002d199922504b039
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-11-03 20:14:13 -0500

    ajustar a moviles 100%

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -16,7 +16,8 @@ import Youtube from "./video_youtube.vue"
 
 
     </div>
-
+    <div class="arrow"></div>
+    <div class="arrow2"></div>
     <div class="logo-redes">
 
         <a class="icon" href="https://t.me/monnerversecommunity" target="_blank">
@@ -188,12 +189,109 @@ img {
 }
 
 /* style mobile */
-@media screen and (max-width: 1370px) {
+@media screen and (max-width: 900px) {
     .video {
         width: 100%;
         min-width: 100%;
         box-sizing: border-box;
-        padding: 0 1rem;
+        padding: 0 5%;
+        max-width: 15rem;
     }
+
+
+    .arrow,
+    .arrow:before {
+        position: absolute;
+        left: 50%
+    }
+
+    .arrow {
+        width: 25px;
+        height: 25px;
+        top: 56%;
+        left: 94%;
+        margin: -20px 0 0 -20px;
+        -webkit-transform: rotate(-50deg);
+        transform: rotate(-50deg);
+        border-left: none;
+        border-top: none;
+        border-right: 4px #ffc000 solid;
+        border-bottom: 4px #ffc000  solid;
+        z-index: 999;
+    }
+
+    .arrow:before {
+        content: '';
+        width: 15px;
+        height: 15px;
+        top: 50%;
+        margin: -10px 0 0 -10px;
+        border-left: none;
+        border-top: none;
+        border-right: 2px #ffc000  solid;
+        border-bottom: 2px #ffc000  solid;
+        animation-duration: 2s;
+        animation-iteration-count: infinite;
+        animation-name: arrow;
+    }
+
+    @keyframes arrow {
+        0% {
+            opacity: 1;
+        }
+
+        100% {
+            opacity: 0;
+            transform: translate(-10px, -10px);
+        }
+    }
+
+    .arrow2,
+    .arrow2:before {
+        position: absolute;
+        left: 50%
+    }
+
+    .arrow2 {
+        width: 25px;
+        height: 25px;
+        top: 56%;
+        left: 9%;
+        margin: -20px 0 0 -20px;
+        -webkit-transform: rotate(135deg);
+        transform: rotate(135deg);
+        border-left: none;
+        border-top: none;
+        border-right: 4px #ffc000  solid;
+        border-bottom: 4px #ffc000  solid;
+        z-index: 999;
+    }
+
+    .arrow2:before {
+        content: '';
+        width: 15px;
+        height: 15px;
+        top: 50%;
+        margin: -10px 0 0 -10px;
+        border-left: none;
+        border-top: none;
+        border-right: 2px #ffc000 solid;
+        border-bottom: 2px #ffc000  solid;
+        animation-duration: 2s;
+        animation-iteration-count: infinite;
+        animation-name: arrow2;
+    }
+
+    @keyframes arrow2 {
+        0% {
+            opacity: 1;
+        }
+
+        100% {
+            opacity: 0;
+            transform: translate(-10px, -10px);
+        }
+    }
+
 }
 </style>
