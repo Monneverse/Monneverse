2e0a4b7115ef753b012778702ffa1d8231551fdc
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-11-13 01:53:36 -0500

    ajustar redes exchagues

--- src/components/Exchanges.vue
+++ src/components/Exchanges.vue
@@ -99,7 +99,7 @@ import ComponenteIconos from "./componenteIconos.vue";
       </div>
     </div>
   </div>
-  <div class="logo_redes">
+  <div class="logo-redes">
     <div class="icon icon-telegram">
       <a href="https://t.me/MonnerverseCripto" target="_blank">
         <img src="../assets/Telegram_logo.svg" alt="logo telegram" srcset="" />
@@ -512,14 +512,13 @@ img {
     margin-right: 0.5%;
   }
 }
-
-.logo_redes {
+.logo-redes {
   position: absolute;
-  bottom: 1%;
-  left: 1%;
-  width: 20%;
-  height: 5%;
-  z-index: 245;
+  bottom: 1rem;
+  left: 1rem;
+  width: 20rem;
+  height: 5rem;
+  z-index: 14;
   display: flex;
   justify-content: start;
 }
@@ -527,8 +526,7 @@ img {
 .icon {
   width: 3rem;
   height: auto;
-  z-index: 18;
-  margin-left: 1%;
+  margin-left: 1rem;
 }
 
 @media screen and (max-width: 900px) {
