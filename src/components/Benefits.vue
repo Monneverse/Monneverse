ecef7b060dd202e23597e4719175f4089f214313
Juan Diego Mejía Maestre <mejiamaestrejuandiego@gmail.com>
2022-12-10 11:38:46 -0500

    arreglar animacion aro en benefit

--- src/components/Benefits.vue
+++ src/components/Benefits.vue
@@ -78,7 +78,7 @@ export default {
       <div class="benefit_title">
         <img class="benefit_img_plus" src="/img/plus.svg" alt="Plus-Benefits">
         <h2 class="title">
-          Benfits for our early investors
+          Benefits for our early investors
         </h2>
       </div>
       <p class="description"> Investors of our Fair Launch will have access to our daily payment of 7.5% of their
@@ -93,128 +93,23 @@ export default {
       </p>
     </div>
 
-    <!--       
-      <div class="benefits">
-        <div
-          :class="{
-            beneficio: true,
-            'animacion-aparecer': this.enterAnimation && !this.isRevert,
-            'animacion-aparecer-revert': this.exitAnimation && this.isRevert,
-            'animacion-desaparecer': this.enterAnimation && this.isRevert,
-            'animacion-desaparecer-revert':
-              this.exitAnimation && !this.isRevert,
-          }"
-        >
-          <img
-            id="a"
-            class="circulos"
-            src="/img/plus.svg"
-            alt="Plus-Benefits"
-          />
-          <div class="content-price">
-            <div class="titulo-two">
-              <h2>
-                <span>$ </span>1,5  <b>USD</b>  in Pinksale
-                <img src="/img/pinksale.png" alt="Pinksale" srcset="" />
-              </h2>
-            </div>
-          </div>
-          <div class="content-text">
-            <div class="sale">
-              <h3>Pre-sale Price</h3>
-            </div>
-          </div>
-        </div>
+    <div :class="{
+      aro: true,
+      'animacion-aparecer': this.enterAnimation && !this.isRevert,
+      'animacion-aparecer-revert': this.exitAnimation && this.isRevert,
+      'animacion-desaparecer': this.enterAnimation && this.isRevert,
+      'animacion-desaparecer-revert': this.exitAnimation && !this.isRevert,
+    }">
+      <img class="aro_superior" src="../assets/aro.svg" alt="Ring" />
 
-        <div
-          :class="{
-            beneficio: true,
-            'animacion-aparecer': this.enterAnimation && !this.isRevert,
-            'animacion-aparecer-revert': this.exitAnimation && this.isRevert,
-            'animacion-desaparecer': this.enterAnimation && this.isRevert,
-            'animacion-desaparecer-revert':
-              this.exitAnimation && !this.isRevert,
-          }"
-        >
-          <img
-            id="b"
-            class="circulos"
-            src="/img/plus.svg"
-            alt="Plus-Benefits"
-          />
-          <div class="content-price">
-            <div class="titulo-two">
-              <h1>Benefits for our early investors</h1>
-            </div>
-          </div>
-          <div class="content-text">
-            <div class="text">
-              <p>Investors of our Fair Launch will have access to our daily payment of 7.5% of their investment.  will get $300 in bonus MNR in our MonnerGame game center (available soon).    And founder privileges in all our projects from $300 and up.  Note: The Wallets that invested in Fair Launch will be published on our official website and will be recognized as a benefited wallet.. remarca las letras importantes con amarillo que se vea cool.</p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          :class="{
-            beneficio: true,
-            'animacion-aparecer': this.enterAnimation && !this.isRevert,
-            'animacion-aparecer-revert': this.exitAnimation && this.isRevert,
-            'animacion-desaparecer': this.enterAnimation && this.isRevert,
-            'animacion-desaparecer-revert':
-              this.exitAnimation && !this.isRevert,
-          }"
-        >
-          <img
-            id="d"
-            class="circulos"
-            src="/img/plus.svg"
-            alt="Plus-Benefits"
-          />
-          <div class="content-price">
-            <div class="titulo-two">
-              <h1>7.5%<span>*</span>24h</h1>
-            </div>
-          </div>
-          <div class="content-text">
-            <div class="sale">
-              <h3>Important</h3>
-            </div>
-
-            <div class="text">
-              <p class="">
-                Note: y en el texto colocamos esto: Staking 7.5% every 48 hours is only for the wallets that invested to our LAUNCH FAIR  🔥
-
-              
-              </p>
-            </div>
-          </div>
-        </div>
-      </div> -->
-
-    <!-- <div
-        :class="{
-          aro: true,
-          'animacion-aparecer': this.enterAnimation && !this.isRevert,
-          'animacion-aparecer-revert': this.exitAnimation && this.isRevert,
-          'animacion-desaparecer': this.enterAnimation && this.isRevert,
-          'animacion-desaparecer-revert': this.exitAnimation && !this.isRevert,
-        }"
-      >
-        <img class="image1" src="../assets/aro.svg" alt="Ring" />
-
-        <img
-          :class="{
-            image: true,
-            animated: true,
-            duration1s: true,
-            fadeInLeft: this.enterAnimation,
-            fadeOutLeft: this.exitAnimation,
-          }"
-          src="../assets/aro.svg"
-          alt="Ring"
-        />
-      </div>
-       -->
+      <img :class="{
+        aro_inferior:true,
+        animated: true,
+        duration1s: true,
+        fadeInLeft: this.enterAnimation,
+        fadeOutLeft: this.exitAnimation,
+      }" src="../assets/aro.svg" alt="Ring" />
+    </div>
   </div>
 
 
@@ -311,7 +206,7 @@ img {
 .contenido .contenido_title {
   color: white;
   font-size: 4rem;
-  margin: 4% 0 0 4%;
+  margin: 3.8% 0 0 4%;
 
 }
 
@@ -433,337 +328,15 @@ img {
   .fondo_benefit {
     display: none;
   }
-}
-
-/* 
-p {
-  text-align: justify;
-}
-
-.display_flex {
-  display: flex;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-}
-
-
-.amarrillo {
-  color: #ffc000;
-  font-weight: 600;
-}
-
-.titulo {
-  position: absolute;
-  width: 100%;
-  top: 4.5rem;
-  text-align: center;
-  z-index: 45;
-}
-
-.titulo h1 {
-  font-size: 2rem;
-  font-family: "Work Sans", sans-serif;
-  font-weight: 600;
-  color: white;
-}
-
-#titulo22 {
-  width: 1005;
-  margin-left: 0;
-  border-radius: 1px solid red;
-}
-
-.benefits {
-  position: absolute;
-  z-index: 14;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: flex-start;
-  flex-direction: column;
-  width: 90%;
-  height: auto;
-  top: 14%;
-}
-
-.beneficio {
-  width: 100%;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  margin-top: 0.5rem;
-}
-
-.beneficio:nth-child(1) {
-  height: 20%;
-}
-
-.beneficio:nth-child(2) {
-  height: 10%;
-}
-
-.beneficio:nth-child(3) {
-  height: 10%;
-}
-
-.beneficio:nth-child(5) {
-  opacity: 0 !important;
-  margin-top: 4.5rem;
-  height: 25rem;
-}
-
-@media screen and (max-width: 900px) {
-  .beneficio:nth-child(4) {
-    display: block;
+  .aro {
+    display: none;
   }
-}
-
-.content-price {
-  width: 100%;
-  height: 3.5rem;
-}
-
-.titulo-benefit {
-  margin: 0;
-  width: 100;
-  height: 100%;
-  background-color: #161431;
-  border-radius: 0.4em;
-}
-
-.titulo-benefit h1 {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  margin: 0;
-  font-size: 1.8rem;
-  margin-left: 5%;
-}
-
-.titulo-benefit h1 span {
-  color: #37f395;
-}
-
-.titulo-benefit h1 b {
-  font-size: 1.5rem;
-}
-
-.titulo-benefit h2 {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  margin: 0;
-  font-size: 1rem;
-  margin-left: 5%;
-}
-
-.info {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  position: relative;
-  top: -10px;
-  left: 5%;
-  font-size: 1.2rem;
-}
-
-.info-two {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  position: relative;
-  top: 0px;
-  left: 15%;
-  font-size: 1.2rem;
-}
-
-.titulo-two {
-  height: 100%;
-  width: 100%;
-  background-color: #161431;
-  display: flex;
-  align-items: center;
-  border-radius: 0.4em;
-}
-
-.titulo-two h1 {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  margin: 0;
-  font-size: 1.3rem;
-  margin-left: 5%;
-}
-
-.titulo-two h2 {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  margin: 0;
-  font-size: 1rem;
-  margin-left: 5%;
-  display: flex;
-  align-items: center;
-}
-
-.titulo-two h2 img {
-  margin-left: 1rem;
-  height: 2rem;
-}
-
-.titulo-two h1 span {
-  color: #37f395;
-}
-
-.content-text {
-  height: 70%;
-  width: 90%;
-  margin-left: 5%;
-}
-
-.content-text h3 {
-  color: white;
-}
-
-.content-text p {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  font-size: 0.7rem;
-}
-
-.sale {
-  font-family: "Work Sans", sans-serif;
-  color: white;
-  font-size: 0.8rem;
-}
-
-.text {
-  height: 0%;
-}
-
-
-
-.scroll {
-  scroll-snap-type: y mandatory;
-  overflow-y: scroll;
-}
-
-.scroll p span {
-  color: yellow;
-}
-
-
-
-
-@media screen and (min-width: 769px) {
-  .fondo_rayos {
-    z-index: -2;
-    left: 0%;
-  }
-
-  .fondo-benefit>img {
-    width: 100%;
-    height: 115vh;
-  }
-
-  .fondo_rayos img {
-    width: 100%;
-  }
-
-  .titulo-two h2 {
-    font-size: 1.5rem;
-  }
-}
-
-@media screen and (min-width: 769px) {
-  .benefits {
-    flex: none;
-    top: 20%;
-    width: 70%;
-    height: 50%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    grid-column-gap: 0;
-    grid-row-gap: 30px;
-    grid-template-areas:
-      "a b"
-      "c d"
-      "e f";
-  }
-
-  .titulo {
-    width: 0;
-    top: 10rem;
-    left: 18%;
-  }
-
-  .titulo h1 {
-    font-size: 4.5vw;
-  }
-
-  .fondo_rayos {
-    display: initial;
-  }
-}
-
-.logo-redes {
-  position: absolute;
-  bottom: 1rem;
-  left: 1rem;
-  width: 20rem;
-  height: 5rem;
-  z-index: 14;
-  display: none;
-  justify-content: start;
-}
-
-.icon {
-  width: 3rem;
-  height: 3rem;
-  margin-left: 1rem;
-}
-
-@media screen and (min-width: 769px) {
-  .logo-redes {
-    display: flex;
-    z-index: 100000;
-  }
-}
-
-.icon img {
-  width: 100%;
-  height: 100%;
-}
-
-.circulos {
-  position: relative;
-  top: 20%;
-  left: -3%;
-  width: 1rem;
-  z-index: 33;
-}
-
-#a {
-  top: 2.5rem;
-}
-
-#b {
-  top: 2.5rem;
-}
-
-#c {
-  top: 1.5rem;
-}
-
-#d {
-  top: 2.5rem;
-}
-
-.circulos img {
-  margin: 0 auto;
-  width: 4%;
-  z-index: 5;
 }
 
 @media screen and (min-width: 769px) {
   .aro {
-    top: -40%;
+    top: 25%;
+    right: 28%;
     display: flex;
     flex-direction: column;
     position: relative;
@@ -774,256 +347,24 @@ p {
   }
 
   .aro img {
-    width: 30%;
+    width: 40%;
     margin-right: 2%;
     margin-bottom: 0;
   }
 
-  .image {
+  .aro_superior {
     position: relative;
     z-index: 1;
-    top: -30%;
+    top: -5%;
   }
-
-  .animacion-aro {
-    animation-name: aro;
-    -webkit-animation-name: aro;
-    animation-duration: 0.5s;
-    -webkit-animation-duration: 0.5s;
-    -webkit-animation-iteration-count: 1;
-    animation-iteration-count: 1;
-    animation-fill-mode: forwards;
-    -webkit-animation-fill-mode: forwards;
-  }
-
-  @keyframes aro {
-    0% {
-      left: -25%;
-    }
-
-    100% {
-      left: 0%;
-    }
-  }
-
-  .circulos {
-    display: flex;
-    flex-direction: column;
+  .aro_inferior {
     position: relative;
-    top: 30%;
-    left: -4%;
-    width: 8%;
-    z-index: 33;
+    z-index: 1;
+    top: -45%;
   }
 
-  #a {
-    top: 30%;
-  }
-
-  #b {
-    top: 30%;
-  }
-
-  #c {
-    top: 30%;
-  }
-
-  #d {
-    top: 30%;
-  }
-
-  .beneficio {
-    width: 80%;
-    height: 90%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .beneficio:nth-child(1) {
-    position: relative;
-    top: 50%;
-    width: 80%;
-    height: 150%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .beneficio:nth-child(2) {
-    width: 80%;
-    height: 150%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .beneficio:nth-child(3) {
-    position: relative;
-    height: 80%;
-    top: 30%;
-  }
-
-  .beneficio:nth-child(4) {
-    height: 80%;
-    margin-top: 0;
-  }
-
-  .content-price {
-    width: 100%;
-    height: 50%;
-  }
-
-  .content-text {
-    width: 100%;
-    height: 50%;
-  }
-
-  .titulo-benefit {
-    margin: 0;
-    width: 100;
-    height: 100%;
-    background-color: #161431;
-    border-radius: 0.4em;
-  }
-
-  .titulo-benefit h1 {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    margin: 0;
-    font-size: 4vw;
-    margin-left: 5%;
-  }
-
-  .titulo-benefit h1 span {
-    color: #37f395;
-  }
-
-  .titulo-benefit h1 b {
-    font-size: 1.8vw;
-  }
-
-  .info {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    position: relative;
-    top: -10px;
-    left: 15%;
-    font-size: 1.5vw;
-  }
-
-  .info-two {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    position: relative;
-    top: 5px;
-    left: 18%;
-    font-size: 1.5vw;
-    height: 15%;
-  }
-
-  .titulo-two {
-    height: 100%;
-    width: 100%;
-    background-color: #161431;
-    display: flex;
-    align-items: center;
-    border-radius: 0.4em;
-  }
-
-  .titulo-two h1 {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    margin: 0;
-    font-size: 4vw;
-    margin-left: 5%;
-  }
-
-  .titulo-two h1 span {
-    color: #37f395;
-  }
-
-  .content-text {
-    height: 50%;
-    width: 100%;
-    margin-left: 5%;
-  }
-
-  .content-text h3 {
-    color: white;
-  }
-
-  .content-text p {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    font-size: 1vw;
-  }
-
-  .sale {
-    font-family: "Work Sans", sans-serif;
-    color: white;
-    font-size: 1.1vw;
-    width: 90;
-  }
-
-  .sale h3 {
-    font-weight: 400;
-  }
-
-  .text {
-    height: 80%;
-  }
-
-  .scroll {
-    scroll-snap-type: initial;
-    overflow-y: hidden;
-  }
 }
 
-@media (min-width: 769px) and (width < 900px) {
-  .beneficio:nth-child(1) {
-    position: relative;
-    top: 50%;
-    width: 80%;
-    height: 80%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .beneficio:nth-child(2) {
-    position: relative;
-    height: 80%;
-    top: 30%;
-  }
-
-  .beneficio:nth-child(4) {
-    margin-top: 0;
-  }
-}
-
-.animacion-caja1 {
-  animation-name: caja1;
-  -webkit-animation-name: caja1;
-  animation-duration: 5s;
-  -webkit-animation-duration: 5s;
-  -webkit-animation-iteration-count: 1;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  -webkit-animation-fill-mode: forwards;
-}
-
-@keyframes caja1 {
-  0% {
-    position: absolute;
-    top: -50%;
-    left: 25%;
-    background-color: white;
-    width: 430px;
-  }
-
-  100% {
-    top: 60%;
-    left: -23%;
-  }
-}
-*/
 
 /* Animaciones */
 .animacion-aparecer {
